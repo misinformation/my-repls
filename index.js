69b70c5fc29092cffc7339fdbd94b866fd626ede
misinformation <kytsuna@gmail.com>
2019-11-10 10:18:10 +0000

    tasks from book

---
+++ index.js
@@ -0,0 +1,12 @@
+var drawCats = function (howManyTimes) {
+for (var i = 0; i < howManyTimes; i++) {
+console.log(i + " =^.^=");
+}
+};
+drawCats(10);
+
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAnHour = secondsInAMinute * minutesInAnHour;
+secondsInAnHour;
+3600
