63a6ede083f80b6cc7f9a8c6b73cd40ae3c8257b
misinformation <kytsuna@gmail.com>
2019-11-10 11:17:21 +0000

    add  secondsInAYear

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-var drawCats = function (howManyTimes) {
-for (var i = 0; i < howManyTimes; i++) {
-console.log(i + " =^.^=");
-}
-};
-drawCats(10);
+//var drawCats = function (howManyTimes) {
+//for (var i = 0; i < howManyTimes; i++) {
+//console.log(i + " =^.^=");
+//}
+//};
+//drawCats(10);
+
 
 var secondsInAMinute = 60;
+console.log ('secondsInAMinute:', secondsInAMinute);
 var minutesInAnHour = 60;
+console.log ('minutesInAnHour:', minutesInAnHour);
 var secondsInAnHour = secondsInAMinute * minutesInAnHour;
-secondsInAnHour;
-3600
+console.log ('secondsInAnHour:', secondsInAnHour);
+var hoursInADay = 24;
+console.log ('hoursInADay:', hoursInADay);
+var secondsInADay = secondsInAnHour * hoursInADay;
+console.log ('secondsInADay:', secondsInADay);
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+console.log('secondsInAYear:', secondsInAYear);
+
+
